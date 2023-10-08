55fb437022bb31b7aab59adb0cd91be0ae6545ec
Dannezzzan <123192307+Dannezzzan@users.noreply.github.com>
2023-10-08 02:03:38 +0200

    Create sv.lua
    Swedish esx_boat translation

---
+++ locales/sv.lua
@@ -0,0 +1,31 @@
+Locales['sv'] = {
+    -- shop
+    ['boat_shop'] = 'Båtaffär',
+    ['boat_shop_open'] = 'Tryck [E] för att öppna båtaffären.',
+    ['boat_shop_confirm'] = 'Köp <span style="color: yellow;">%s</span> för <span style="color: orange;">%skr</span>?',
+    ['boat_shop_bought'] = 'Du har köpt en %s för %skr',
+    ['boat_shop_nomoney'] = 'Du har inte ~r~råd med denna båt!',
+    ['confirm_no'] = 'Nej',
+    ['confirm_yes'] = 'Ja',
+  
+    -- garage
+    ['garage'] = 'Båtgarage',
+    ['garage_open'] = 'Tryck [E] för att öppna båtgaraget.',
+    ['garage_store'] = 'Tryck [E] för att parkera båten i garaget.',
+    ['garage_taken'] = 'Båten har tagits ut!',
+    ['garage_stored'] = 'Båten har parkerats i garaget!',
+    ['garage_noboats'] = 'Du har inga parkerade båtar! Besök båtaffären för att köpa en.',
+    ['garage_blocked'] = 'Båten kan inte tas ut för att någon blockerar spawn!',
+    ['garage_notowner'] = 'Du äger inte denna båt!',
+  
+    -- license
+    ['license_menu'] = 'Köp båtlicens?',
+    ['license_buy_no'] = 'Nej',
+    ['license_buy_yes'] = 'Köp båtlicens <span style="color: green;">%skr</span>',
+    ['license_bought'] = 'Du har köpt båtlicens för %skr',
+    ['license_nomoney'] = 'Du har inte ~r~råd med båtlicens!',
+  
+    -- blips
+    ['blip_garage'] = 'Båtgarage',
+    ['blip_shop'] = 'Båtaffär',
+  }
